74797912d20f55d07d2ddf3f8b817e6b1a8e2c64
unknown <you@example.com>
2019-05-05 22:44:33 -0300

    lista_4

--- lista_4.R
+++ lista_4.R
@@ -1,17 +1,26 @@
 ## Exercícios Lista 4
 
 # 1. Com o RStudio, para o desenvolvimento da lista 4 crie um projeto com versionamento no GitHub.
-Apresente o link do repositório do projeto como resposta ao exercício. Nele, deverá constar o script de
-entrega da lista.
+  #Apresente o link do repositório do projeto como resposta ao exercício. Nele, deverá constar o script de
+  #entrega da lista.
 
-
+# https://github.com/walterra007/lista_4/blob/master/lista_4.R
 
 # 2. O Secretário de Educação do Estado de Pernambuco deseja saber se há correlação entre o número de
-alunos por docente nos municípios do Estado e o IDH-M. Utilize as bases de dados do Censo Escolar de
-2016 e do PNUD (com dados de 2010) em sua resposta. Para que o resultado seja aceitável, cumpra os
-seguintes requisitos:
+  #alunos por docente nos municípios do Estado e o IDH-M. Utilize as bases de dados do Censo Escolar de
+  #2016 e do PNUD (com dados de 2010) em sua resposta. Para que o resultado seja aceitável, cumpra os
+  #seguintes requisitos:
   
-  # • Use a função read_xlsx do pacote readxl para carregar os dados do PNUD; 
+  # • Use a função read_xlsx do pacote readxl para carregar os dados do PNUD;
+
+setwd("c:/dados/dados_encontro_2_ufpe")
+# carregando arquivos CENSO ESCOLAR 2016
+load("matricula_pe_censo_escolar_2016.RData")
+load("docentes_pe_censo_escolar_2016.RData")
+load("turmas_pe_censo_escolar_2016.RData")
+load("escolas_pe_censo_escolar_2016.RData")
+
+
   # • Não deve haver docente com mais de 70 anos ou com menos de 18 anos;
   # • Não deve haver aluno com mais de 25 anos ou com menos de 1 ano;
   # • Apresente estatísticas descritivas do número de alunos por docente nos municípios do Estado;
